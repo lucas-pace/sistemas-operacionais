3d8ff125a13bb84af1cf607e1c94547e06a495c4
andrecaetanov <andrecvidal@hotmail.com>
2019-07-14 15:17:48 -0300

    myfsInfo criado e instalacao do fs implementada

--- myfs.c
+++ myfs.c
@@ -1,7 +1,7 @@
 /*
-*  myfs.c - Implementacao de fimcpes do sistema de arquivos
+*  myfs.c - Implementacao de funcoes do sistema de arquivos
 *
-*  Autores: Lucas de Pace, André Caetano e Cristiano
+*  Autores: Lucas de Pace, André Caetano e Cristiano Nascimento
 *  Projeto: Trabalho Pratico II - Sistemas Operacionais
 *  Organizacao: Universidade Federal de Juiz de Fora
 *  Departamento: Dep. Ciencia da Computacao
@@ -9,29 +9,117 @@
 *  
 */
 
+#include <stdlib.h>
+#include <string.h>
 #include "myfs.h"
 #include "disk.h"
+#include "vfs.h"
 
+int myfsSlot = -1;
 
-/*
-char fsid; // Identificador do tipo de sistema de arquivos
-char *fsname; // Nome do tipo de sistema de arquivos
-int (*isidleFn) (Disk *d);
-int (*formatFn) (Disk *d, unsigned int blockSize);
-int (*openFn) (Disk *d, const char *path);
-int (*readFn) (int fd, char *buf, unsigned int nbytes);
-int (*writeFn) (int fd, const char *buf, unsigned int nbytes);
-int (*closeFn) (int fd);
-int (*opendirFn) (Disk *d, const char *path);
-int (*readdirFn) (int fd, char *filename, unsigned int *inumber,
-unsigned int enumber);
-int (*linkFn) (int fd, const char *filename, unsigned int inumber);
-int (*unlinkFn) (int fd, const char *filename);
-int (*closedirFn) (int fd);
+FSInfo const myfsInfo = {
+    1,
+    "myfs",
+    myfsIsIdle,
+    formatDisk,
+    openFile,
+    readFile,
+    writeFile,
+    closeFile,
+    openDir,
+    readDir,
+    link,
+    unlink,
+    closeDir
+};
 
- */
+File *openFiles[MAX_FDS] = {};
 
+int installMyFS () {
+    myfsSlot = vfsRegisterFS(&myfsInfo);
+    return myfsSlot;
+}
 
-int installMyFS()
-{
+int myfsIsIdle (Disk *disk) {
+    for (int i = 0; i < MAX_FDS; i++) {
+        File *file = openFiles[i];
+        if (file != NULL && diskGetId(disk) == diskGetId(file->disk))
+            return 0;
+    }
+
+    return 1;
+}
+
+int formatDisk (Disk *disk, unsigned int blockSize) {
+    int numBlocks = -1;
+
+    //TODO implementar formatDisk
+
+    return numBlocks;
+}
+
+int openFile (Disk *disk, const char *path) {
+    int fd = -1;
+
+    File *file = malloc(sizeof(File));
+    file->disk = disk;
+
+    //TODO implementar openFile
+
+    return fd;
+}
+
+int readFile (int fd, char *buf, unsigned int nbytes) {
+    int numBytesRead = 0;
+
+    //TODO implementar redFile
+
+    return numBytesRead;
+}
+
+int writeFile (int fd, const char *buf, unsigned int nbytes) {
+    //TODO implementar writeFile
+
+    return -1;
+}
+
+int closeFile (int fd) {
+    File *file = openFiles[fd];
+    if (file == NULL)
+        return -1;
+
+    openFiles[fd] = NULL;
+    free(file);
+
+    return 0;
+}
+
+int openDir (Disk *disk, const char *path) {
+    //TODO implementar openDir
+
+    return -1;
+}
+
+int readDir (int fd, char *filename, unsigned int *inumber) {
+    //TODO implementar readDir
+
+    return -1;
+}
+
+int link (int fd, const char *filename, unsigned int inumber) {
+    //TODO implementar link
+
+    return -1;
+}
+
+int unlink (int fd, const char *filename) {
+    //TODO implementar unlink
+
+    return -1;
+}
+
+int closeDir (int fd) {
+    //TODO implementar closeDir
+
+    return -1;
 }
--- myfs.h
+++ myfs.h
@@ -13,8 +13,37 @@
 #define MYFS_H
 
 #include "disk.h"
+#include "inode.h"
+
+typedef struct file {
+    Disk *disk;
+    Inode *inode;
+    const char *path;
+} File;
 
 //Funcao para instalar um novo sistema de arquivos no S.O.
 int installMyFS ( void );
+
+int myfsIsIdle (Disk *disk);
+
+int formatDisk (Disk *disk, unsigned int blockSize);
+
+int openFile (Disk *disk, const char *path);
+
+int readFile (int fd, char *buf, unsigned int nbytes);
+
+int writeFile (int fd, const char *buf, unsigned int nbytes);
+
+int closeFile (int fd);
+
+int openDir (Disk *disk, const char *path);
+
+int readDir (int fd, char *filename, unsigned int *inumber);
+
+int link (int fd, const char *filename, unsigned int inumber);
+
+int unlink (int fd, const char *filename);
+
+int closeDir (int fd);
 
 #endif
